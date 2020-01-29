35fae67f19cf399a1d3e25751f6d07516a5f61f1
llGuy <luc.rosenzweig@yahoo.com>
2020-01-29 22:58:58 +0000

    Worked on event sytsem

--- source/event_system.hpp
+++ source/event_system.hpp
@@ -7,52 +7,52 @@
 #define MAX_EVENTS 20
 
 enum receiver_t
-{
-	WORLD,
-	GUI,
-	PARTICLES,
-	AUDIO, // TODO: In the future
-	INVALID_RECEIVER
-};
+    {
+     WORLD,
+     GUI,
+     PARTICLES,
+     AUDIO, // TODO: In the future
+     INVALID_RECEIVER
+    };
 
 enum event_type_t 
-{ 
-	REQUEST_TO_JOIN_SERVER,
-	ENTER_SERVER_WORLD,
-	OPEN_MENU,
-	EXIT_MENU,
-	OPEN_CONSOLE,
-	EXIT_CONSOLE,
-	REQUEST_USERNAME,
-	ENTERED_USERNAME,
-	INVALID_EVENT_TYPE
-};
+    { 
+     REQUEST_TO_JOIN_SERVER,
+     ENTER_SERVER_WORLD,
+     OPEN_MENU,
+     EXIT_MENU,
+     OPEN_CONSOLE,
+     EXIT_CONSOLE,
+     REQUEST_USERNAME,
+     ENTERED_USERNAME,
+     INVALID_EVENT_TYPE
+    };
 
 struct event_data_request_to_join_server_t
 {
-	const char *ip_address;
+    const char *ip_address;
 };
 
 struct event_t
 {
-	event_type_t type;
+    event_type_t type;
 
-	// Likely to be allocated on free list allocator
-	void *data;
+    // Likely to be allocated on free list allocator
+    void *data;
 };
 
 struct event_receiver_list_t
 {
-	uint32_t listening_receivers = 0;
+    uint32_t listening_receivers = 0;
 
-	event_t events[receiver_t::INVALID_RECEIVER] = {};
-	stack_dynamic_container_t<receiver_t, receiver_t::INVALID_RECEIVER> event_indices;
+    event_t events[receiver_t::INVALID_RECEIVER] = {};
+    stack_dynamic_container_t<receiver_t, receiver_t::INVALID_RECEIVER> event_indices;
 };
 
 struct event_dispatcher_t
 {
-	event_receiver_list_t event_map[event_type_t::INVALID_EVENT_TYPE] = {};
+    event_receiver_list_t event_map[event_type_t::INVALID_EVENT_TYPE] = {};
 
-	void subscribe(event_type_t type, receiver_t receiver);
-	void dispatch_event(event_type_t type, void *data);
+    void subscribe(event_type_t type, receiver_t receiver);
+    void dispatch_event(event_type_t type, void *data);
 };
